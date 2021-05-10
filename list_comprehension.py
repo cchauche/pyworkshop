b95e619e7c6c755770bc5266bb535867b86af415
Colin Chauche <colin.chauche@gmail.com>
2021-05-09 22:04:28 -0600

    Add generator comprehension

--- list_comprehension.py
+++ list_comprehension.py
@@ -1,4 +1,7 @@
 names = ["Colin", "Daniel", "Mariel", "Eva", "Katya"]
-# print list with lengths of each name
 
+# print list with lengths of each name
 print([len(name) for name in names])
+
+# print even number names
+print([name for name in names if (len(name) % 2) == 0])
---
+++ other_comprehensions.py
@@ -0,0 +1,4 @@
+gen_squares = (i ** 2 for i in range(0, 10, 2))
+
+for num in gen_squares:
+  print(num)
